0b60826b8c386138b1d3684c40f1e52ff9be8aa8
DawidASM <dawid_kapusta@outlook.com>
2019-12-13 23:11:25 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.SimpleCalculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania opragramowania. ");
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int result1 = calculator.add(2,2);
+        int result2 = calculator.subtract(100,999);
+
+        if(result1 == 4){
+            System.out.println("The result is correct.");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if(result2 == 849){
+            System.out.println("The result is correct.");
+        } else {
+            System.out.println("Error!");
+        }
+
+
     }
 }

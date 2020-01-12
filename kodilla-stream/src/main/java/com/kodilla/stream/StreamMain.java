18b4f20ae375b9df96981a62a9037f1ca49d2ab5
DawidASM <dawid_kapusta@outlook.com>
2020-01-12 19:52:34 +0100

    New Forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,13 @@
 package com.kodilla.stream;
 
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
+
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -18,7 +15,7 @@ public class StreamMain {
     public static void main(String[] args) {
         Forum theForum = new Forum();
 
-        Map<Integer, Forum> theMapOfUsers = theForum.getTheUserList().stream()
+        Map<Integer, ForumUser> theMapOfUsers = theForum.getTheUserList().stream()
                 .filter(forumUser -> forumUser.getSex()=='M')
                 .filter(forumUser -> Period.between(forumUser.getDateOfBirth(),LocalDate.now()).getYears() > 20)
                 .filter(forumUser -> forumUser.getPostQuantity() >= 1)
